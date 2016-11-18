ef59c6ee8e29fb5bc95a3f72f62702c345c4c202
Alejandro Marano <alejandromarano08@gmail.com>
2016-11-18 04:27:05 -0300

    18-Nov-4.26hs
    -siguen las pruebas

--- PREHEAT/main.c
+++ PREHEAT/main.c
@@ -8,8 +8,6 @@
  *SE BUSCA IMPLEMENTAR:
  *-PID (NADA MENOS :P )
  *
- *
- *
  *  Adelante y suerte
  */
 
@@ -29,6 +27,7 @@
 
 void Delay(__IO uint32_t nTime);  //funcion Delay que usa SysTick
 int devolver_temperatura_en_grados(); // funcion PHinclude
+void color_segun_temperatura();
 void TIM_Config(void);
 
 TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;   //variable para el timer y PWM
@@ -51,7 +50,7 @@ int main(void)
 
 	UB_LCD_2x16_Init(); // inicializa el display 16x2
 
-    //declarar_leds();     // GPIO leds pin 11 12 13 14
+    declarar_leds();     // GPIO leds pin 11 12 13 14
 
 	//declarar_boton();    // GPIO boton
 
@@ -69,10 +68,13 @@ int main(void)
 
 	char stringtemperatura[4]; // String donde se guarda la temperatura
 
+	 /* Compute the prescaler value */
+	  PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 2800) - 1; // 28000000
+
 	//PWM PWM PWM PWM PWM PWM PWM PWM PMW
 	/* Time base configuration */
 	  TIM_TimeBaseStructure.TIM_Period = 499; //499
-	  TIM_TimeBaseStructure.TIM_Prescaler = 2000;
+	  TIM_TimeBaseStructure.TIM_Prescaler =100000 ;
 	  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
 	  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
@@ -144,13 +146,13 @@ void color_segun_temperatura()
 
 				int32_t temperaturaengrados=devolver_temperatura_en_grados();
 
-	    	    if(temperaturaengrados<27)   //menor de 27 grados enciende el led VERDE
+	    	    if(temperaturaengrados<270)   //menor de 27 grados enciende el led VERDE
 	    	    {
 	    	    	GPIO_SetBits(GPIOD,GPIO_Pin_12);
 	    	    	GPIO_ResetBits(GPIOD,GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
 	    	    }
 
-	    	    if(temperaturaengrados>=27) //Mayor o igual a  27 grados enciende el led ROJO
+	    	    if(temperaturaengrados>=270) //Mayor o igual a  27 grados enciende el led ROJO
 	    	       {
 	    	    	GPIO_SetBits(GPIOD,GPIO_Pin_13);
 	    	       	GPIO_ResetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_14|GPIO_Pin_15);
